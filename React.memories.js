6b95580682507dbfddeacaa01b3fb9910b7fe815
Maximiliano Céspedes <maxicespedes7@hotmail.com>
2016-05-20 07:41:06 -0300

    Update React.memories.js

--- React.memories.js
+++ React.memories.js
@@ -1,6 +1,6 @@
 React Notes
 
-#2
+#2 -Components and rendering
 Render()
   -Only returns one tag
       return(
@@ -13,13 +13,38 @@ Render()
         <h1> Hi im {name}</h1>
   Inside handlevars you can execute almost any JS 
         <h1> Hi im {this.myMethod(param)}</h1>
-        
-  -Bercause its a class you can have a constructor method (always execute super() first)
+
+--Inside class 
+  Because its a class you can have a constructor method (always execute super() first)
     {
       constructor(){
         super();
         this.myName = "maxi";
       }
     }
+    
+      then you can refer to that variable like this.myName (remember THIS.)
   
+    
+#3 -Composing Multiple React.js
+
+  1- Create components folder
+  2- Create Layout.js (Components names should be capitalized)
+  3- export default class (no reactdom neccessary)
+  4- import class in the parent
+  5- Create Header.js component
+  6- import class in the Layout.js
+  
+  -If we want to render multiple elements we can create a list inside the render method with components inside and then we can call it by {}
+    render(){
+      var list =[<component/>,<component/>,<component/>];
+      return(
+        {list}
+        );
+      
+    }
+  -If a components have components in it you can create a folder with the name of the component and put their inner components there.
+    ex.
+      Header
+        Title
     
